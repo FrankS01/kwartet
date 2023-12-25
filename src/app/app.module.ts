ea557afe6281c3a9d3a0126ec69ca26ef56052f9
Frank <frankstam01@gmail.com>
2023-12-25 20:05:20 +0100

    New homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { ShellComponent } from './components/pages/shell/shell.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { FooterComponent } from './components/organisms/footer/footer.component';
 
 @NgModule({
   declarations: [
     ShellComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
