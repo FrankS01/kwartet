e87926502e61fbdc8c98af1b13e44064aaf44158
Frank <frankstam01@gmail.com>
2024-01-14 20:52:40 +0100

    Start using PrimeNG and remove custom button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,9 @@ import { ShellComponent } from './components/pages/shell/shell.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { FooterComponent } from './components/organisms/footer/footer.component';
 import { GamesComponent } from './components/pages/games/games.component';
-import { ButtonComponent } from './components/atoms/button/button.component';
 import { HeaderComponent } from './components/organisms/header/header.component';
+import { ButtonModule } from "primeng/button";
+import { RippleModule } from "primeng/ripple";
 
 @NgModule({
   declarations: [
@@ -15,12 +16,13 @@ import { HeaderComponent } from './components/organisms/header/header.component'
     HomeComponent,
     FooterComponent,
     GamesComponent,
-    ButtonComponent,
     HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    RippleModule
   ],
   providers: [],
   bootstrap: [ShellComponent]
--- src/app/components/pages/shell/shell.component.ts
+++ src/app/components/pages/shell/shell.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from "primeng/api";
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss']
 })
-export class ShellComponent {
+export class ShellComponent implements OnInit {
   title = 'Kwartet';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
